a66210d2bf20ae23f8f73d46a64cfb75ed578404
Haitam-Elgharras <elgharras.haitam@gmail.com>
2023-06-11 15:27:02 +0100

    init

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const items = document.querySelectorAll(".item");
+const counter = document.querySelector(".counter");
+
+window.onload = () => {
+  count();
+  counter.addEventListener("click", count);
+};
+
+items.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    e.currentTarget.style.backgroundColor = "white";
+    const after = e.currentTarget.querySelector(".after");
+    after.classList.remove("active");
+    after.style.display = "none";
+    count();
+  });
+});
+
+const markAll = document.querySelector(".mark-read");
+markAll.addEventListener("click", (e) => {
+  items.forEach((item) => {
+    item.style.backgroundColor = "white";
+    const after = item.querySelector(".after");
+    after.classList.remove("active");
+    after.style.display = "none";
+    count();
+  });
+});
+
+function count() {
+  actives = 0;
+  items.forEach((item) => {
+    const after = item.querySelector(".after");
+    if (after.classList.contains("active")) {
+      actives++;
+    }
+  });
+  counter.innerHTML = actives;
+}
